d8c437912e0ac049bb1c12458b10539269c74e81
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-09-12 22:23:09 -0700

    added title

--- components/Forms/Forms.tsx
+++ components/Forms/Forms.tsx
@@ -6,11 +6,17 @@ import { FC } from "react";
 const Forms: FC<ChildProps> = ({ uuid, socket, setUser }) => {
   return (
     <div className="flex w-full h-screen justify-center items-center">
-      <div className="w-1/2 h-full flex flex-col gap-6 justify-center items-center">
+      <div className="w-1/2 h-full relative flex flex-col gap-6 justify-center items-center">
+        <h1 className="absolute top-0 right-0 py-2 px-2 text-3xl font-bold">
+          Strata
+        </h1>
         <h2 className="text-2xl font-semibold">Create Room</h2>
         <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser} />
       </div>
-      <div className="w-1/2 h-full flex flex-col gap-6 justify-center items-center bg-[#1677FF] text-white">
+      <div className="w-1/2 h-full relative flex flex-col gap-6 justify-center items-center bg-[#1677FF] text-white">
+        <h1 className="absolute top-0 left-0 py-2 px-2 text-3xl font-semibold text-white">
+          A real-time collaborative whiteboard
+        </h1>
         <h2 className="text-2xl font-semibold">Join Room</h2>
         <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
       </div>
